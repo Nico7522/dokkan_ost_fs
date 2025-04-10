daf57fb8ca8ccb6f62fd87ca55c9b04b3e311fd4
Nicolas <nico.daddabbo7100@gmail.com>
2025-04-10 15:36:12 +0200

    ajout d'un nouveau logo

--- dokkan_ost_front/src/app/app.config.ts
+++ dokkan_ost_front/src/app/app.config.ts
@@ -1,17 +1,9 @@
-import {
-  ApplicationConfig,
-  importProvidersFrom,
-  provideZoneChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
-import {
-  HttpClientModule,
-  provideHttpClient,
-  withFetch,
-} from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [

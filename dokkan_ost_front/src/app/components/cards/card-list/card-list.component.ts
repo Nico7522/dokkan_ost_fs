26fe35c58039f867be9016a8529fb9c9e9bfedf8
Nicolas <nico.daddabbo7100@gmail.com>
2025-04-26 12:33:03 +0200

    ajout de la pagination côté angular presque ok

--- dokkan_ost_front/src/app/components/cards/card-list/card-list.component.ts
+++ dokkan_ost_front/src/app/components/cards/card-list/card-list.component.ts
@@ -1,16 +1,16 @@
-import { Component, inject, PLATFORM_ID, input } from '@angular/core';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
 import { CardsService } from '../../../services/cards/cards.service';
 import { NgxSpinnerComponent, NgxSpinnerService } from 'ngx-spinner';
 import { map } from 'rxjs';
 import { keysToCamel } from '../../../helpers/helpers';
-import { AsyncPipe, isPlatformBrowser } from '@angular/common';
+import { isPlatformBrowser } from '@angular/common';
 import { CardComponent } from '../../../shared/card/card.component';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { Card } from '../../../models/card';
 
 @Component({
   selector: 'app-cards',
-  imports: [AsyncPipe, CardComponent, RouterOutlet, NgxSpinnerComponent],
+  imports: [CardComponent, RouterOutlet, NgxSpinnerComponent],
   templateUrl: './card-list.component.html',
   styleUrl: './card-list.component.scss',
 })
@@ -19,20 +19,31 @@ export class CardsComponent {
   isServer = isPlatformBrowser(this.plateformId);
 
   // Resolver
-  cards = input<Card[]>([]);
+  // cards = input<Card[]>([]);
   private readonly cardService = inject(CardsService);
   private readonly spinnerService = inject(NgxSpinnerService);
-  cardData$ = this.cardService.getCards().pipe(
-    map((result) => {
-      console.log(result.total, result.nbPage);
-      this.spinnerService.show('cards');
-      let c = result.data.map((c) => keysToCamel(c));
+  private readonly router = inject(Router);
+  // cardData$ = this.cardService.getCards().pipe(
+  //   map((result) => {
+  //     this.spinnerService.show('cards');
+  //     let cards = result.data.map((c) => keysToCamel(c));
+  //     return {
+  //       cards,
+  //       total: result.total,
+  //       nbPage: result.nbPage,
+  //     };
+  //   })
+  // );
 
-      return {
-        cards: c,
-        total: result.total,
-        nbPage: result.nbPage,
-      };
-    })
-  );
+  result = this.cardService.cards.value;
+  setPage(pageNumber: number) {
+    this.updateQueryParams({ page: pageNumber.toString() });
+    this.cardService.setPage(pageNumber.toString());
+  }
+  private updateQueryParams(queryParams: Record<string, string>) {
+    this.router.navigate([], {
+      queryParams,
+      queryParamsHandling: 'merge',
+    });
+  }
 }
--- dokkan_ost_front/src/app/services/cards/cards.service.ts
+++ dokkan_ost_front/src/app/services/cards/cards.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { inject, Injectable, signal } from '@angular/core';
+import { map, Observable, tap } from 'rxjs';
 import { Card, CardDetails } from '../../models/card';
 import { environment } from '../../../environments/environment';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { keysToCamel } from '../../helpers/helpers';
 
 @Injectable({
   providedIn: 'root',
@@ -21,4 +23,31 @@ export class CardsService {
   getCardById(id: number): Observable<CardDetails> {
     return this.httpClient.get<CardDetails>(`${this.apiUrl}/cards/${id}`);
   }
+
+  private page = signal('1');
+  private name = signal('');
+  setPage(pageNumber: string) {
+    this.page.set(pageNumber);
+  }
+  cards = rxResource({
+    request: () => ({
+      page: this.page(),
+      name: this.name(),
+    }),
+    loader: ({ request }) =>
+      this.httpClient
+        .get<{ data: Card[]; nbPage: number; total: number }>(
+          `${this.apiUrl}/cards?page=${request.page}&name=${request.name}`
+        )
+        .pipe(
+          map((result) => {
+            let cards = result.data.map((c) => keysToCamel(c));
+            return {
+              cards,
+              total: result.total,
+              nbPage: result.nbPage,
+            };
+          })
+        ),
+  });
 }
